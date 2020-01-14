45a3df9e13b63bbe90c2340bf21f174b2a83e8b8
zhouyusen <zhouyusen>
2020-01-14 16:51:46 +0800

    zzz

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,14 @@
 <template>
-    <div style="height:1080px;width:1920px;text-align:left;">
+    <div style="height:1080px;width:1920px;text-align:left;" class="mapBox">
         <div id="basicMapbox" style="height:1080px;width:100%;"></div>
-        <!--<div id="three"></div>-->
+        <div id="three"></div>
     </div>
 </template>
 
 <script>
     import mapboxgl from 'mapbox-gl'
     import * as THREE from 'three'
+    import OrbitControls from 'three/examples/js/controls/OrbitControls'
     import GLTFLoader from 'three-gltf-loader';
 
     export default {
@@ -26,6 +27,7 @@
         mounted() {
             this.init2();
             this.init();
+            this.init3();
         },
         methods: {
             // 初始化
@@ -134,11 +136,8 @@
                             .multiply(rotationZ);
 
                         this.camera.projectionMatrix = m1.multiply(m2);
-
-                        this.renderer.state.reset();
-
                         this.scene.add(that.scene);
-
+                        this.renderer.state.reset();
                         this.renderer.render(this.scene,this.camera);
                         this.map.triggerRepaint();
                     }
@@ -160,6 +159,64 @@
                 cube.position.set(20,0,20);
                 this.scene.add( cube );
             },
+            init3(){
+                var scene = new THREE.Scene();
+                // var geometry = new THREE.SphereGeometry(60, 40, 40); //创建一个球体几何对象
+                var geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个立方体几何对象Geometry
+                var material = new THREE.MeshLambertMaterial({
+                    color: 0x0000ff
+                }); //材质对象Material
+                var mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
+                scene.add(mesh); //网格模型添加到场景中
+                /**
+                 * 光源设置
+                 */
+                    //点光源
+                var point = new THREE.PointLight(0xffffff);
+                point.position.set(400, 200, 300); //点光源位置
+                scene.add(point); //点光源添加到场景中
+                //环境光
+                var ambient = new THREE.AmbientLight(0x444444);
+                scene.add(ambient);
+                // console.log(scene)
+                // console.log(scene.children)
+                /**
+                 * 相机设置
+                 */
+                var width = window.innerWidth; //窗口宽度
+                var height = window.innerHeight; //窗口高度
+                var k = width / height; //窗口宽高比
+                var s = 200; //三维场景显示范围控制系数，系数越大，显示的范围越大
+                //创建相机对象
+                var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+                camera.position.set(200, 300, 200); //设置相机位置
+                camera.lookAt(scene.position); //设置相机方向(指向的场景对象)
+                /**
+                 * 创建渲染器对象
+                 */
+                var renderer = new THREE.WebGLRenderer({antialias: true,alpha:true});
+                renderer.setSize(width,height);//设置渲染区域尺寸
+                // renderer.setClearColor('rgba(0,0,0,0)',1); //设置背景颜色
+                renderer.domElement.style.pointerEvents='none'
+                document.getElementsByClassName("mapBox")[0].appendChild(renderer.domElement); //body元素中插入canvas对象
+                //执行渲染操作   指定场景、相机作为参数
+                // renderer.render(scene, camera)
+                //间隔20ms周期性调用函数fun,20ms也就是刷新频率是50FPS(1s/20ms)，每秒渲染50次
+                // setInterval(()=>{
+                //     renderer.state.reset();
+                //     renderer.render(scene,camera);//执行渲染操作
+                //     mesh.rotateY(0.01);//每次绕y轴旋转0.01弧度
+                // },20);
+
+                function render() {
+                    renderer.render(scene,camera);//执行渲染操作
+                }
+                render();
+                if(camera){
+                    var controls = new OrbitControls(camera,renderer.domElement);//创建控件对象
+                    controls.addEventListener('change',render);//监听鼠标、键盘事件
+                }
+            }
         }
     }
 </script>
